7172d8b37d649e19c56dab28eb2916ba75def307
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2020-11-10 08:01:03 +0700

    Add files via upload

---
+++ Streaming_Film.py
@@ -0,0 +1,244 @@
+import selenium
+import time
+import sys, os
+import datetime
+import webbrowser
+
+os.system('color B')
+print('''                                                                        
+ @@@@@@   @@@@@@@  @@@@@@@   @@@@@@@@   @@@@@@   @@@@@@@@@@    @@@@@@   
+@@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@   
+!@@         @@!    @@!  @@@  @@!       @@!  @@@  @@! @@! @@!  !@@       
+!@!         !@!    !@!  @!@  !@!       !@!  @!@  !@! !@! !@!  !@!       
+!!@@!!      @!!    @!@!!@!   @!!!:!    @!@!@!@!  @!! !!@ @!@  !!@@!!    
+ !!@!!!     !!!    !!@!@!    !!!!!:    !!!@!!!!  !@!   ! !@!   !!@!!!   
+     !:!    !!:    !!: :!!   !!:       !!:  !!!  !!:     !!:       !:!  
+    !:!     :!:    :!:  !:!  :!:       :!:  !:!  :!:     :!:      !:!   
+:::: ::      ::    ::   :::   :: ::::  ::   :::  :::     ::   :::: ::   
+:: : :       :      :   : :  : :: ::    :   : :   :      :    :: : :    
+                                                                        
+                         
+@@@  @@@  @@@  @@@@@@@   
+@@@  @@@  @@@  @@@@@@@@  
+@@!  @@@  @@!  @@!  @@@  
+!@!  @!@  !@!  !@!  @!@  
+@!@  !@!  !!@  @!@  !@!  
+!@!  !!!  !!!  !@!  !!!  
+:!:  !!:  !!:  !!:  !!!  
+ ::!!:!   :!:  :!:  !:!  
+  ::::     ::   :::: ::  
+   :      :    :: :  :   
+                         
+
+''')
+print("LOADING...")
+time.sleep(8.5)
+os.system('cls')
+
+print("Streaming Film Online")
+dork = input("Masukan Judul film : ")
+jawab = input(" Sudah Benar ? [Y/N] ? : ")
+if jawab=="y" or jawab=="Y":
+    print("Ready for 3s")
+    time.sleep(1)
+    print("Ready for 2s")
+    time.sleep(1)
+    print("Ready for 1s")
+    time.sleep(0.5)
+    webbrowser.open(f"https://www.indoxx1.ltd/?s="+dork)
+    webbrowser.open(f"http://103.194.171.205/?s="+dork, new=0)
+elif jawab=="n" or jawab=="N":
+    print("Next...")
+    time.sleep(2)
+    pass
+else:
+    print("Pilihannya hanya Y/N.")
+    pass
+
+a = str(input("Anda Ingin Part 2 Nya ? [Y/N] : "))
+if a=="y" or a=="Y":
+    print("Ready For 3s Again yeah...")
+    time.sleep(1)
+    print("Ready For 2s Again yeah..")
+    time.sleep(1)
+    print("Ready For 1s Again yeah.")
+    time.sleep(1)
+    webbrowser.open(f"https://www.popcornflix.com/searchs?q="+dork)
+    webbrowser.open(f"https://filmapik.host/?s="+dork, new=0)
+    webbrowser.open(f"https://bioskopkeren.cool/?s="+dork, new=1)
+    pass
+elif a=="n" or a=="N":
+    print("Next...")
+    time.sleep(2)
+    pass
+else:
+    print("Pilihannya Hanya Y/N...")
+    pass
+
+ch = str(input("Anda Ingin menonton Film bioskop yang Bergenre ? [Y/N] "))
+if ch == 'Y' or ch == 'y':
+    print("Oke Kamu Pilih Genre ingin Nomor berapa ?\n")
+    print("1. Serial TV") #https://www.indoxxi.id/series/
+    print("2. Fiksi Ilmiah") #https://www.indoxx1.ltd/genre/science-fiction/
+    print("3. Aksi")
+    print("4. Petualangan")
+    print("5. Keluarga")
+    print("6. Drama")
+    print("7. Animasi")
+    print("8. Thriller")
+    print("9. Fantasy")
+    print("10. Komedi")
+    print("11. Crime/Kriminal")
+    print("12. Movie")
+    print("13. Series")
+    print("14. Anime\n")
+    option = int(input("Masukan Nomor: "))
+    if option==1:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxxi.id/series/")
+        pass
+    elif option==2:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/science-fiction/")
+        pass
+    elif option==3:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/action/")
+        pass
+    elif option==4:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/adventure/")
+        pass
+    elif option==5:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/family/")
+        pass
+    elif option==6:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/drama/")
+        pass
+    elif option==7:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/animation/")
+        pass
+    elif option==8:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/thriller/")
+        pass
+    elif option==9:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/fantasy/")
+        pass
+    elif option==10:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/comedy/")
+        pass
+    elif option==11:
+        time.sleep(0.5)
+        print("Sedang Diarahkan...")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/genre/crime/")
+        pass
+    elif option==12:
+        time.sleep(0.5)
+        print("Sedang Diarahkan")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/movies/", new=0)
+        pass
+    elif option==13:
+        time.sleep(0.5)
+        print("Sedang Diarahkan")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/series/", new=0)
+        pass
+    elif option==14:
+        time.sleep(0.5)
+        print("Sedang Diarahkan")
+        time.sleep(3)
+        webbrowser.open("https://www.indoxx1.ltd/anime/",new=0)
+        pass
+elif ch=="n" or ch=="N":
+    time.sleep(1)
+    print("Terima Kasih Jika Tidak Ingin :D ")
+    time.sleep(6)
+    print("\n")
+    pass
+else:
+    print("Maaf pilihannya Hanya Y/N...")
+    pass
+
+os.system('pause')
+os.system('cls')
+os.system('color B')
+print("\nIngin Bergabung Ke Streaming dan Download Film kami ?\n")
+print("Anda Bisa Menjadi Upload Video, Kontributor, atau Pemberi Komentar Atau Penonton Saja...\n")
+print("Ini aman 100% Secure, tenang Ini Via Google Drive, kalian bisa Request Film\n")
+print("Jika Kalian Bingung silahkan Tanya Ke Admin (Saya) Gmail dibawah")
+print("\n")
+print("======================")
+print("xnuversh1kar4@gmail.com")
+print("======================")
+print("\n")
+
+lo = str(input("Anda Ingin Menghubungi Admin ? [Y/N] : "))
+if lo=="y" or lo=="Y":
+    print("Sedang Diarahkan tunggu Sebentar...")
+    time.sleep(5)
+    webbrowser.open(f"https://mail.google.com/mail/mu/mp/409/#co")
+    hm = str(input("Ketik Y untuk mailto: (ngirim pesan tanpa google) "))
+    if hm=="y" or hm=="Y":
+        time.sleep(1)
+        webbrowser.open(f"mailto:xnuversh1kar4@gmail.com")
+        pass
+    elif hm=="n" or hm=="N":
+        a = "Program ini akan terhenti dalam 3 detik".upper()
+        print(a)
+        time.sleep(3)
+        pass
+    else:
+        print("Hanya ada pilihan Y/N. terimakasih...")
+        pass
+        
+elif lo=="n" or lo=="N":
+    print("Terima Kasih...\n")
+    pass
+else:
+    print("Hanya ada pilihan Y/N")
+    pass
+
+print("Anda Melewati pilihan Menghubungi Admin")
+ol = str(input("Anda ingin Mendaftar gratis streaming dan Download via Google Drive ? [Y/N] : "))
+if ol=="Y" or ol=="y":
+    print("redirecting, Sedang diarahkan ke Arah pendaftaran...")
+    time.sleep(2)
+    webbrowser.open(f"https://forms.gle/AytRCMq72ZGSN1SN7",new=0)
+    pass
+else:
+    print("Hanya ada Pilihan Y/N")
+    pass
+
+print("Terima Kasih Sudah Memakai Program Ini\n")
+print("Source code : @indradwi.25\n")
+print("github: https://github.com/Xnuvers007/\n")
+time.sleep(3)
+exit(code=None)
